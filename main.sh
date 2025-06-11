8eda90ffec661207d3aacc96c7a5ba56488a8988
qiuxiuya <95994462+qiuxiuya@users.noreply.github.com>
2025-06-12 02:10:12 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -7,11 +7,11 @@ echo """\
  / / / /  __/>  <(__  ) /_/ /_/ / /  / / / / / /
 /_/ /_/\___/_/|_/____/\__/\____/_/  /_/ /_/ /_/ 
 _________________________________________________
-        magicTCP v0.1 | 25/09/2023 edition
+        magicTCP v0.1 | 12/06/2025 edition
 
 """
 
-install_magictcp_kernel(){
+install_magictcp_kernel() {
     magictcp_kernel_version="6.1.55-magictcp001"
     apt install -y wget
     wget "https://raw.githubusercontent.com/qiuxiuya/magicTCP/main/kernel/linux-headers-${magictcp_kernel_version}_${magictcp_kernel_version}-4_amd64.deb" -O "linux-headers-${magictcp_kernel_version}.deb"
@@ -26,7 +26,7 @@ install_magictcp_kernel(){
     reboot
 }
 
-apply_tcp_optimization(){
+apply_tcp_optimization() {
     declare -A params=(
         ["net.ipv4.tcp_rmem"]="8192 262144 536870912"
         ["net.ipv4.tcp_wmem"]="8192 262144 536870912"
@@ -40,14 +40,20 @@ apply_tcp_optimization(){
         ["net.ipv4.ip_forward"]="1"
         ["net.ipv4.conf.all.forwarding"]="1"
         ["net.ipv4.conf.default.forwarding"]="1"
-        # experimental UDP optimisation
         ["net.ipv4.udp_rmem_min"]="16384"
         ["net.ipv4.udp_wmem_min"]="16384"
-        ["net.core.rmem_default"]="26214400"
-        ["net.core.rmem_max"]="26214400"
+        ["net.core.rmem_default"]="262144"
+        ["net.core.rmem_max"]="2621440"
+        ["net.core.wmem_default"]="262144"
+        ["net.core.wmem_max"]="2621440"
         ["net.core.optmem_max"]="65535"
-        ["net.ipv4.udp_mem"]="8192 262144 536870912"
+        ["net.ipv4.udp_mem"]="8192 262144 524288"
         ["net.core.netdev_max_backlog"]="30000"
+        ["net.ipv4.tcp_fastopen"]="3"
+        ["net.ipv4.tcp_mtu_probing"]="1"
+        ["net.ipv4.tcp_mem"]="786432 1048576 26777216"
+        ["net.ipv4.tcp_tw_reuse"]="1"
+        ["net.ipv4.tcp_fin_timeout"]="15"
     )
 
     cp /etc/sysctl.conf /etc/sysctl.conf.bak
@@ -55,18 +61,16 @@ apply_tcp_optimization(){
         if grep -q "^$param" /etc/sysctl.conf; then
             sed -i "s|^$param.*|$param = ${params[$param]}|" /etc/sysctl.conf
         else
-            # If the parameter doesn't exist, add it
-            echo "$param = ${params[$param]}" >> /etc/sysctl.conf
+            echo "$param = ${params[$param]}" >>/etc/sysctl.conf
         fi
     done
 
-    echo "3" > /proc/sys/net/ipv4/tcp_fastopen
     sysctl -p
-    
-grep -q '^precedence ::ffff:0:0/96  100' /etc/gai.conf || echo 'precedence ::ffff:0:0/96  100' >> /etc/gai.conf
+
+    grep -q '^precedence ::ffff:0:0/96  100' /etc/gai.conf || echo 'precedence ::ffff:0:0/96  100' >>/etc/gai.conf
 }
 
-uninstall_other_kernels(){
+uninstall_other_kernels() {
     sudo dpkg --list | grep linux-image | grep -v 'magic' | awk '{print $2}' | xargs sudo apt-get remove --purge -y
     sudo update-grub
 }
