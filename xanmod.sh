ca91f3bb99df090937a9736211034d68f84842f0
qiuxiuya <95994462+qiuxiuya@users.noreply.github.com>
2026-01-20 15:06:42 +0800

    Update xanmod.sh

--- xanmod.sh
+++ xanmod.sh
@@ -48,7 +48,8 @@ reboot
 apply_tcp_optimization() {
     declare -A params=(
         ["net.ipv4.tcp_congestion_control"]="bbr"
-        ["net.core.default_qdisc"]="cake"
+        ["net.core.default_qdisc"]="fq"
+        ["net.ipv4.tcp_fastopen"]="3"
     )
 
     cp /etc/sysctl.conf /etc/sysctl.conf.bak
