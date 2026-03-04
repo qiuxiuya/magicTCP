fe260e7e3e95be76fe05f492fff232468463786c
qiuxiuya <95994462+qiuxiuya@users.noreply.github.com>
2026-03-04 22:00:31 +0800

    Update xanmod.sh

--- xanmod.sh
+++ xanmod.sh
@@ -51,8 +51,25 @@ reboot
 
 apply_tcp_optimization() {
     declare -A params=(
-        ["net.ipv4.tcp_congestion_control"]="bbr"
+        ["net.ipv4.tcp_rmem"]="8192 262144 536870912"
+        ["net.ipv4.tcp_wmem"]="8192 262144 536870912"
+        ["net.ipv4.tcp_collapse_max_bytes"]="6291456"
+        ["net.ipv4.tcp_notsent_lowat"]="131072"
+        ["net.ipv4.tcp_adv_win_scale"]="-1"
         ["net.core.default_qdisc"]="fq"
+        ["net.ipv4.tcp_congestion_control"]="bbr"
+        ["net.ipv4.tcp_window_scaling"]="1"
+        ["net.ipv4.conf.all.route_localnet"]="1"
+        ["net.ipv4.ip_forward"]="1"
+        ["net.ipv4.conf.all.forwarding"]="1"
+        ["net.ipv4.conf.default.forwarding"]="1"
+        ["net.ipv4.udp_rmem_min"]="16384"
+        ["net.ipv4.udp_wmem_min"]="16384"
+        ["net.core.rmem_default"]="26214400"
+        ["net.core.rmem_max"]="26214400"
+        ["net.core.optmem_max"]="65535"
+        ["net.ipv4.udp_mem"]="8192 262144 536870912"
+        ["net.core.netdev_max_backlog"]="30000"
     )
 
     cp /etc/sysctl.conf /etc/sysctl.conf.bak
